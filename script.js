4c3dbac400cde5386df6aebba8e4d401397bd463
MaAs0706e <aswanthmadhav07@gmail.com>
2025-06-26 23:34:02 +0530

    feat : add the reference btw the login and signup button with their ccorresponding pages

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginp').addEventListener('click', () =>{
+    window.location.href = 'login.html'
+})
+
+
+document.getElementById('signupp').addEventListener('click', () =>{
+    window.location.href = 'signup.html'
+})
