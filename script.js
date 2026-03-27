bed21b482d90635554a90ba54ceb286fadde52bd
MaAs0706e <aswanthmadhav07@gmail.com>
2026-03-27 23:02:31 +0530

    starting over

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('loginp').addEventListener('click', () =>{
-    window.location.href = 'login.html'
-})
-
-
-document.getElementById('signupp').addEventListener('click', () =>{
-    window.location.href = 'signup.html'
-})
